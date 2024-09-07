d4c7eedada098fdc1c39a2b849f0a95d0a98e2b5
John Vial <badwolf.johnnyv@gmail.com>
2024-09-07 17:35:44 +0800

    Adjusted for krytn namespacing

--- launch/real_robot.launch.py
+++ launch/real_robot.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
 
     # Step 1. Process robot file. 
     robot_file = join(get_package_share_directory("krytn"), "robot_description","krytn","krytn.urdf.xacro")
-    robot_xml = Command(["xacro ",robot_file])
+    robot_xml = Command(["xacro ",robot_file, " ","gazebo:=False"])
 
     #Step 2. Publish robot file to ros topic /robot_description & static joint positions to /tf
     robot_state_publisher = Node(
@@ -25,7 +25,7 @@ def generate_launch_description():
         output='both',
         parameters=[{'robot_description':robot_xml, 
                      }],
-        
+        namespace="/krytn"        
     )
 
     # Step 5: Enable the ros2 controllers
@@ -33,14 +33,16 @@ def generate_launch_description():
                 package="controller_manager",
                 executable="ros2_control_node",
                 output="screen",
-                parameters=[join(get_package_share_directory('krytn'),'config','diffdrive_control.yaml')]
+                parameters=[join(get_package_share_directory('krytn'),'config','diffdrive_control.yaml')],
+                namespace="/krytn"
+
             )
 
     # Step 5: Enable the ros2 controllers
     start_controllers  = Node(
                 package="controller_manager",
                 executable="spawner",
-                arguments=[ '-c', "/controller_manager",
+                arguments=[ '-c', "/krytn/controller_manager",
                     'joint_state_broadcaster', 'diff_drive_base_controller'],
                 output="screen",
             )
